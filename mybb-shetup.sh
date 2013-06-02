e59b7dae65da7b8b2a7a469412c73e14f35a92a4
Josh Harmon <harmon.josh99@gmail.com>
2013-06-02 14:46:33 -0700

    Revert "DB stuff and testing on dev server"
    This reverts commit 4a3091481a2a5c3478e9cae2240aac725e4091f9.

--- mybb-shetup.sh
+++ mybb-shetup.sh
@@ -195,44 +195,23 @@ files_chmod() {
     chmod 666 inc/settings.php
 }
 
-mysql_setup() {
+database_create() {
     info "To create a database for you, we need some high-level privileges temporarily."
-    info "Attempting to find the MySQL executable... Please wait."
-    sleep 1
-
-    mysql_location
-    if [ -f $mysql_executable ]; then
-        mysql_run_commands; #It works. Continue.
-    else
-        mysql_location #Try again
-    fi
-}
-
-mysql_location() {
-    if command_exists mysql; then
-        mysql_executable="mysql"
-    else
-        echo "We could not find the location of the MySQL executable. We need some help here."
-        prompt_input "Where is the MySQL executable located? Full path required." mysql_executable
-    fi
-}
-
-mysql_run_commands() {
 
     # Get root pass
     prompt_input "We assume your MySQL server has a root user, what is its password?" "" root_pass
     
     # Create database
     prompt_input "What should the name of the database be? It should not already exist." "mybb" db_name
-    $mysql_executable -uroot -p $root_pass -e "CREATE DATABASE '$db_name';"
+    mysql -uroot -p $root_pass -e "CREATE DATABASE '$db_name';"
 
     # Create database user
     prompt_input "What should the username be for the regular DB user that MyBB will use?" "mybb" db_user
     prompt_input "Great! What should its password be? []: " db_pass
-    $mysql_executable -uroot -p $root_pass -e "CREATE USER '$db_user'@'localhost' IDENTIFIED BY '$db_pass';"
+    mysql -uroot -p $root_pass -e "CREATE USER '$db_user'@'localhost' IDENTIFIED BY '$db_pass';"
 
     # Grant permissions to database user
-    $mysql_executable -uroot -p $root_pass -e "GRANT ALL ON $db_name.* TO '$db_user'@'localhost';"
+    mysql -uroot -p $root_pass -e "GRANT ALL ON $db_name.* TO '$db_user'@'localhost';"
 }
 
 php_server_start() {
@@ -264,11 +243,6 @@ main() {
     files_unfold
     config_rename
     files_chmod
-    if prompt_yn "Do you want to set up the MySQL database for MyBB too?" "Y"; then
-        mysql_setup
-    else
-        return 0;
-    fi
 }
 
 main "$@"
